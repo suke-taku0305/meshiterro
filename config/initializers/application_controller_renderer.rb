39f2f91661fe11eb372d5aaaefbff3fd5dcc92fd
suke-taku0305 <takkurumi0305@icloud.com>
2020-03-22 23:14:07 +0900

    [finish]meshiterro

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '115ba7f6e9000d136c5fd1cf42694bd6b782f575282ead07e777777a1f5c526167010bc49196d2efde5a70c314bfd615fe491efdf4a7cc9a24ff7249000d8d3c'
